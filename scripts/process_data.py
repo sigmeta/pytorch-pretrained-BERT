fcb6d9d506970c09b5e627c5bac199fb1236b684
Sigmeta <sigmeta@pku.edu.cn>
2019-03-18 13:33:09 +0800

    change data

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -11,44 +11,48 @@ wpaths=plist
 phones=set()
 train=[]
 test=[]
+
+def get_data(char, path, split):
+    DOMTree = xml.dom.minidom.parse(path)
+    collection = DOMTree.documentElement
+    sis = collection.getElementsByTagName("si")
+    for si in sis:
+        js_data = {'id': si.getAttribute('id')}
+        js_data['text'] = si.childNodes[1].childNodes[0].data
+        js_data['position'] = -1
+        js_data['char']=char
+        for i, w in enumerate(js_data['text']):
+            if w == char:
+                js_data['position'] = i
+        # cut the text if too long
+        if js_data['position'] > 500:
+            js_data['text'] = js_data['text'][js_data['position'] - 400:]
+            js_data['position'] = 400
+        assert js_data['position'] != -1
+        assert js_data['text'][js_data['position']]==char
+        for w in si.getElementsByTagName("w"):
+            if w.getAttribute('v') == char:
+                js_data['phone'] = w.getAttribute('p')
+        assert 'phone' in js_data.keys()
+        phones.add(js_data['phone'])
+        if split=="train":
+            train.append(js_data)
+        elif split=="test":
+            test.append(js_data)
+        else:
+            raise Exception("split should be in ['train','test']")
+
+
 for n,wpath in enumerate(wpaths):
     print("processing",wpath)
     #train data
-    DOMTree = xml.dom.minidom.parse(os.path.join(data_path,wpath,"training.xml"))
-    collection = DOMTree.documentElement
-    sis = collection.getElementsByTagName("si")
-    for si in sis:
-        js_data = {'id': si.getAttribute('id')}
-        js_data['text'] = si.childNodes[1].childNodes[0].data
-        js_data['position'] = -1
-        for i, w in enumerate(js_data['text']):
-            if w == wlist[n]:
-                js_data['position'] = i
-        assert js_data['position'] != -1
-        for w in si.getElementsByTagName("w"):
-            if w.getAttribute('v') == wlist[n]:
-                js_data['phone'] = w.getAttribute('p')
-        assert 'phone' in js_data.keys()
-        train.append(js_data)
-        phones.add(js_data['phone'])
+    get_data(wlist[n], os.path.join(data_path,wpath,"training.xml"),'train')
+    if os.path.exists(os.path.join(data_path,wpath,"training.News.xml")):
+        get_data(wlist[n], os.path.join(data_path, wpath, "training.News.xml"), 'train')
     #test data
-    DOMTree = xml.dom.minidom.parse(os.path.join(data_path, wpath, "test.xml"))
-    collection = DOMTree.documentElement
-    sis = collection.getElementsByTagName("si")
-    for si in sis:
-        js_data = {'id': si.getAttribute('id')}
-        js_data['text'] = si.childNodes[1].childNodes[0].data
-        js_data['position'] = -1
-        for i, w in enumerate(js_data['text']):
-            if w == wlist[n]:
-                js_data['position'] = i
-        assert js_data['position'] != -1
-        for w in si.getElementsByTagName("w"):
-            if w.getAttribute('v') == wlist[n]:
-                js_data['phone'] = w.getAttribute('p')
-        assert 'phone' in js_data.keys()
-        test.append(js_data)
-        phones.add(js_data['phone'])
+    get_data(wlist[n], os.path.join(data_path, wpath, "test.xml"), 'test')
+
+
 #save
 with open("../data/train.json",'w',encoding='utf8') as f:
     f.write(json.dumps(train))
